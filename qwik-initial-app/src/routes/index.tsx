952db85873b321ee1aec0e47d68ab5f75823a534
Adrian <adrian.talonia11@gmail.com>
2023-07-17 22:48:35 -0600

    clean project

--- qwik-initial-app/src/routes/index.tsx
+++ qwik-initial-app/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Counter from "~/components/starter/counter/counter";
@@ -7,15 +7,25 @@ import Infobox from "~/components/starter/infobox/infobox";
 import Starter from "~/components/starter/next-steps/next-steps";
 
 export default component$(() => {
+
+  const pokemonId: number = useSignal(1); //primitivos, booleans, strings
+
+
+
   return (
     <>
-    <span class="text-9xl">Hi world</span>
+      <span class="text-2xl">Buscador simple</span>
+      <span class="text-9xl">{pokemonId}</span>
+      <div>
+        <button class="btn btn-primary mr-2">Anterior</button>
+        <button class="btn btn-primary">Siguiente</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "PokeQwik",
   meta: [
     {
       name: "description",
--- qwik-initial-app/src/routes/layout.tsx
+++ qwik-initial-app/src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "~/components/starter/header/header";
+import Navbar from "~/components/shared/navbar/navbar";
 import Footer from "~/components/starter/footer/footer";
 
 import styles from "./styles.css?inline";
@@ -28,7 +28,7 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
+      <Navbar />
       <main>
         <Slot />
       </main>
